794df17ac461a01b0100d2651de523b3dde27bcd
Rogerio de Leon Pereira <vinashu@gmail.com>
2016-09-20 15:53:56 -0300

    Criado diretiva Accordion

---
+++ js/uiAccordionDirective.js
@@ -0,0 +1,36 @@
+angular.module("ciesd").directive("uiAccordions", function(){
+    return {
+        controller: function($scope, $element, $attrs){
+            var accordions = [];
+            this.registerAccordion = function(accordion){
+                accordions.push(accordion);
+            }
+            this.closeAll = function(){
+                accordions.forEach(function(accordion){
+                    accordion.isOpened = false;
+                });
+            }
+        }
+    };
+});
+angular.module("ciesd").directive("uiAccordion", function(){
+    return {
+        templateUrl: "view/accordion.html",
+        transclude: true,
+        scope: {
+            title: "@"
+        },
+        require: "^uiAccordions",
+        link: function(scope, element, attrs, ctrl){
+            ctrl.registerAccordion(scope);
+            scope.open = function(){
+                if (scope.isOpened == true){
+                    scope.isOpened = false;
+                } else {
+                    ctrl.closeAll();                    
+                    scope.isOpened = true;
+                }
+            };
+        }
+    };
+});
