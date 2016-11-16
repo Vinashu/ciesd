1bc9d58dd2f3b8521f14054a76f0419282ec49d6
Rogerio de Leon Pereira <vinashu@gmail.com>
2016-11-16 16:30:59 -0200

    Corrigo warning quando data vazia

--- js/uiDateDirective.js
+++ js/uiDateDirective.js
@@ -3,13 +3,15 @@ angular.module("ciesd").directive("uiDate", function(){
         require: "ngModel",
         link: function(scope, element, attrs, ctrl){
             var _formatDate = function(date){
-                date = date.replace(/[^0-9]+/g, "");
-                if(date.length > 2){
-                    date = date.substring(0,2) + "/" + date.substring(2);
-                }
-                if(date.length > 5){
-                    date = date.substring(0,5) + "/" + date.substring(5,9);
-                }                
+                    if (date != undefined) {
+                    date = date.replace(/[^0-9]+/g, "");
+                    if(date.length > 2){
+                        date = date.substring(0,2) + "/" + date.substring(2);
+                    }
+                    if(date.length > 5){
+                        date = date.substring(0,5) + "/" + date.substring(5,9);
+                    }   
+                }             
                 return date;
             };
             element.bind("keyup", function(){
